c3b3fcf84f97fd97fdb271d3b7375119f33acb77
ffffffffffffffffuck <fap51schfra040@osz-teltow.de>
2026-09-16 14:29:29 +0200

    Add files via upload

--- kodieren.py
+++ kodieren.py
@@ -6,18 +6,22 @@ Created on Wed Sep 16 12:24:39 2026
 @author: franka
 """
 
-print("Dieses Programm verschlüsstelt einen Text mit dem Caesar-Chiffre. Der verschlüsselte Text wird nur in Kleinbuchstaben ausgegeben.")
-eingabe = input("Geben Sie einen Text zum Verschlüsseln ein.\n\n")
-step = int(input("Geben Sie eine Zahl zwischen 1 und 25 zur Verschlüsselung an.\n\n"))
+def kodieren(text, text_neu, step):
+    for buchstabe in text:  
+        if buchstabe.isalpha():
+            stelle = ord(buchstabe)
+            stelle_neu = (stelle + step - 97) % 26 + 97 
+            buchstabe = chr(stelle_neu)
+        text_neu = text_neu + buchstabe
+    return text_neu
 
-text = eingabe.lower()
-text_neu = ""
+def main():
+    print("Dieses Programm verschlüsstelt einen Text mit dem Caesar-Chiffre. Der verschlüsselte Text wird nur in Kleinbuchstaben ausgegeben.")
+    eingabe = input("Geben Sie einen Text zum Verschlüsseln ein.\n\n")
+    step = int(input("Geben Sie eine Zahl zwischen 1 und 25 zur Verschlüsselung an.\n\n"))
 
-for buchstabe in text:
-    if buchstabe.isalpha():
-        stelle = ord(buchstabe)
-        stelle_neu = (stelle + step - 97) % 26 + 97 
-        buchstabe = chr(stelle_neu)
-    text_neu = text_neu + buchstabe
-
-print(text_neu)
+    text = eingabe.lower()
+    text_neu = ""
+    print(kodieren(text, text_neu, step))
+    
+main()
